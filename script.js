9bdd7f36e6c7bf7c0d04a0f1ffc0bd9c068903cd
Bruno Minelli <minellibruno@hotmail.com>
2021-12-13 14:50:31 -0300

    Correção do requisito 03 e finalização do requisito 04.

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ const buttonCreateLetter = document.getElementById('criar-carta');
 const inputLetterText = document.getElementById('carta-texto');
 const outputLetter = document.getElementById('carta-gerada');
 
-/*  */
+/* Função: createLetter
+-- Gera a carta ao clicar no botão Criar Carta */
 function createLetter() {
   buttonCreateLetter.addEventListener('click', () => {
-    const letterContent = inputLetterText.value;
-    const arrayWords = letterContent.split(' ');
-    for (let index = 0; index < arrayWords.length; index += 1) {
-      const elementSpan = document.createElement('span');
-      console.log(index);
-      elementSpan.innerHTML = arrayWords[index];
-      outputLetter.appendChild(elementSpan);
-    }
+    if (inputLetterText.value !== '' || !inputLetterText.value) {
+      outputLetter.innerText = '';
+      const letterContent = inputLetterText.value;
+      const arrayWords = letterContent.split(' ');
+      for (let index = 0; index < arrayWords.length; index += 1) {
+        const elementSpan = document.createElement('span');
+        console.log(index);
+        elementSpan.innerHTML = arrayWords[index];
+        outputLetter.appendChild(elementSpan);
+      }
+    } else outputLetter.innerText = 'por favor, digite o conteúdo da carta.';
   });
 }
 createLetter();
