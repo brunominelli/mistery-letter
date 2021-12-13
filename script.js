67df4d4f7fdfa05d8737a508dde92986321f2189
Bruno Minelli <minellibruno@hotmail.com>
2021-12-13 17:44:30 -0300

    Requisito 16 finalizado e entrega dos requisitos obrigatórios do projeto.

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 const buttonCreateLetter = document.getElementById('criar-carta');
 const inputLetterText = document.getElementById('carta-texto');
 const outputLetter = document.getElementById('carta-gerada');
+const styleArray = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig', 'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+const classes = styleArray.length;
 
 /* Função: createLetter
 -- Gera a carta ao clicar no botão Criar Carta */
 console.log(inputLetterText.value);
+console.log(styleArray[Math.floor(Math.random() * classes)]);
 
 function createLetter() {
   buttonCreateLetter.addEventListener('click', () => {
@@ -18,6 +21,7 @@ function createLetter() {
       for (let index = 0; index < arrayWords.length; index += 1) {
         const elementSpan = document.createElement('span');
         elementSpan.innerText = arrayWords[index];
+        elementSpan.classList.add(styleArray[Math.floor(Math.random() * classes)]);
         outputLetter.appendChild(elementSpan);
       }
     }
